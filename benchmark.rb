16487e895a330e61955de1796efad4f5d34b0fa5
Ken Alex Fassone <nexusventuri@gmail.com>
2018-07-15 23:03:40 +0100

    Cleanup

--- benchmark.rb
+++ benchmark.rb
@@ -1,9 +1,10 @@
-require_relative 'require'
-n = 1002
+require_relative 'lib/requires'
+n = 100_002
 
 json_string = File.read('derivative_with_options.json')
 
 #Bench::Parsing.run(json_string, n)
 #Bench::Creation.run(json_string, n)
-Bench::Access.run(json_string, n)
+#Bench::Access.run(json_string, n)
+Bench::CreationAndAccess.run(json_string, n)
 
---
+++ lib/requires.rb
@@ -0,0 +1,19 @@
+require 'ostruct'
+require 'oj'
+require 'benchmark'
+require 'json'
+require_relative 'open_object'
+require_relative 'enhanced_open_object'
+
+require 'concurrent'
+require 'thread'
+require 'open-uri'
+require_relative 'promise_open_object'
+
+require 'benchmark/memory'
+require 'benchmark/ips'
+require_relative 'bench/parsing'
+require_relative 'bench/access'
+require_relative 'bench/creation'
+require_relative 'bench/creation_and_access'
+
